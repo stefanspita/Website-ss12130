b9f471a32cb980b082422e4afb5d401358dd6f6c
Stefan <stefanspita@hotmail.com>
2014-08-16 21:21:35 +0100

    authentication implemented using passport js for admin user to update data is now done

--- server2.js
+++ server2.js
@@ -7,17 +7,74 @@ var https = require('https');
 var privateKey  = fs.readFileSync('key.pem', 'utf8');
 var certificate = fs.readFileSync('cert.cer', 'utf8');
 var getDataFunction = require("./getData");
+var hbs = require("hbs");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var sql = require("sqlite3");
+var cookieParser = require('cookie-parser');
+var methodOverride = require("method-override");
+var session = require("express-session");
 
+//set up authentication
+function findById(id, fn) {
+    var db = new sql.Database("football.db");
+    db.all("SELECT * FROM users WHERE id = ? ", [id], function(err, user){
+        db.close();
+        user = user[0];
+        if(user) return fn(null, user);
+        return fn(new Error('User ' + id + ' does not exist'));
+    });
+}
+
+function findByUsername(username, fn) {
+    var db = new sql.Database("football.db");
+    db.all("SELECT * FROM users WHERE username = ? ", [username], function(err, user){
+        db.close();
+        user = user[0];
+        if(user) fn(null, user);
+        else fn(null, null);
+    });
+}
+
+function ensureAuthenticated(request, response, next) {
+    if (request.isAuthenticated()) { return next(); }
+    response.redirect('/admin');
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        process.nextTick(function () {
+            findByUsername(username, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                return done(null, user);
+            })
+        });
+    }
+));
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(express.static(__dirname + '/app'));
 app.use('/node_modules', express.static(path.join(__dirname, "node_modules")));
+app.set('view engine', 'hbs');
+app.use(cookieParser());
+app.use(methodOverride());
+app.use(session({ secret: 'keyboard cat', resave:false, saveUninitialized:true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-app.get('/', function(request, response) {
-    response.render("index");
-});
 
 app.get("/getData", function(request, response){
     getDataFunction(function(err, data) {
@@ -25,6 +82,20 @@ app.get("/getData", function(request, response){
         response.json(data);
     });
 });
+
+app.get('/admin', function(request, response) {
+    response.render("login");
+});
+
+app.post('/admin', passport.authenticate('local', { failureRedirect: '/admin' }),
+    function(request, response) {
+        response.redirect('/addData');
+});
+
+app.get("/addData", ensureAuthenticated, function(request,response){
+    response.render("addData");
+});
+
 
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
